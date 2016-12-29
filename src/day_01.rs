f1e2ef0a664adf9a45cba32461c2c55a997b0bed
Matthew Piziak <matthew.piziak@gmail.com>
2016-12-28 22:45:50 -0500

    add tests and try_many_from method

--- src/day_01.rs
+++ src/day_01.rs
@@ -1,19 +1,27 @@
 use std::convert::TryFrom;
 
+#[derive(Debug, PartialEq, Eq)]
 enum Turn {
     Left,
     Right,
 }
 
+#[derive(Debug, PartialEq, Eq)]
 struct Instruction {
     turn: Turn,
     blocks: u32,
 }
 
-impl TryFrom<String> for Instruction {
-    type Err = String;
+impl<'a> Instruction {
+    fn try_many_from(s: &'a str) -> Result<Vec<Self>, &'a str> {
+        s.split(", ").map(Self::try_from).collect()
+    }
+}
 
-    fn try_from(s: String) -> Result<Self, String> {
+impl<'a> TryFrom<&'a str> for Instruction {
+    type Err = &'a str;
+
+    fn try_from(s: &'a str) -> Result<Self, &'a str> {
         use self::Turn::*;
         let mut chars = s.chars();
         let turn = match chars.next() {
@@ -21,25 +29,84 @@ impl TryFrom<String> for Instruction {
                 match turn_char {
                     'L' => Left,
                     'R' => Right,
-                    _ => return Err("Turn character invalid".into()),
+                    _ => return Err("Turn character invalid"),
                 }
             }
-            None => return Err("Instruction string is empty".into()),
+            None => return Err("Instruction string is empty"),
         };
         let blocks = match chars.next() {
             Some(blocks_char) => {
                 match blocks_char.to_digit(10) {
                     Some(digit) => digit,
                     None => {
-                        return Err("Could not parse blocks digit".into());
+                        return Err("Could not parse blocks digit");
                     }
                 }
             }
-            None => return Err("Blocks character not found".into()),
+            None => return Err("Blocks character not found"),
         };
         Ok(Self {
             turn: turn,
             blocks: blocks,
         })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_parse_instruction_success() {
+        let instruction = Instruction::try_from("L1").unwrap();
+        assert_eq!(instruction,
+                   Instruction {
+                       turn: Turn::Left,
+                       blocks: 1,
+                   });
+        let instruction = Instruction::try_from("R2").unwrap();
+        assert_eq!(instruction,
+                   Instruction {
+                       turn: Turn::Right,
+                       blocks: 2,
+                   });
+    }
+
+    #[test]
+    fn test_parse_instruction_invalid_turn_character() {
+        let instruction = Instruction::try_from("S1");
+        assert_eq!(instruction, Err("Turn character invalid"));
+    }
+
+    #[test]
+    fn test_parse_instruction_empty_string() {
+        let instruction = Instruction::try_from("");
+        assert_eq!(instruction, Err("Instruction string is empty"));
+    }
+
+    #[test]
+    fn test_parse_instruction_missing_blocks_digit() {
+        let instruction = Instruction::try_from("L");
+        assert_eq!(instruction, Err("Blocks character not found"));
+    }
+
+    #[test]
+    fn test_parse_instruction_invalid_blocks_digit() {
+        let instruction = Instruction::try_from("LL");
+        assert_eq!(instruction, Err("Could not parse blocks digit"));
+    }
+
+    #[test]
+    fn test_parse_instructions() {
+        let instructions = Instruction::try_many_from("L1, R2").unwrap();
+        assert_eq!(instructions,
+                   vec![Instruction {
+                            turn: Turn::Left,
+                            blocks: 1,
+                        },
+                        Instruction {
+                            turn: Turn::Right,
+                            blocks: 2,
+                        }]);
     }
 }
